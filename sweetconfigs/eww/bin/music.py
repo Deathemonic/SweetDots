4eb92e681852c89bad3a8e2cb9311bb76469ce96
ヒヤシンス <deathemonic@gmail.com>
2022-11-01 12:09:27 +0800

    Minor Changes (Temporary Script)
    * Changes: v2.0.2r1
    
    * Added More code in music.py

--- sweetconfigs/eww/bin/music.py
+++ sweetconfigs/eww/bin/music.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env --split-string=python -u
 
+from PIL.Image import init
 from gi import require_version
 require_version("Playerctl", "2.0")
 from gi.repository import GLib, Playerctl
-from utils import config
+from requests import get
+from shutil import copyfileobj
+from utils import config, color_img
 
 
 def player_metadata(*args):
@@ -32,9 +35,8 @@ def player_metadata(*args):
             [','.join(metadata['xesam:artist'][:-1]), metadata['xesam:artist'][-1]]
         )
 
-    if name == 'spotify' and \ 
-            'mpris:trackid' in args[0].props.metadata.keys() and \
-            'ad' in args[0].props.metadata['mpris:trackid']:
+    if 'mpris:trackid' in args[0].props.metadata.keys() and \
+            '/ad/' in args[0].props.metadata['mpris:trackid']:
         metadata['xesam:title'] = 'Advertisement'
         metadata['xesam:artist'] = 'Free Plan'
     
@@ -43,7 +45,56 @@ def player_metadata(*args):
         and fallback_cover not in metadata['mpris:artUrl']
     ):
         metadata['mpris:artUrl'] = None
+
+
+def player_listen(name):
+    player = Playerctl.Player.new_from_name(name)
+    player.connect('metadata', player_metadata, manager)
+    player.connect('playback-status::playing', play, manager)
+    player.connect('playback-status::paused', play, manager)
+    manager.manage_player(player)
     
 
+def player_null_check(player_manager) -> bool:
+    if not len(player_manager.props.player_names):
+        metadata = {
+            'mpris:artUrl': fallback_cover,
+            'xesam:artist': 'Unavailable',
+            'xesam:title': 'Unavailable',
+            'xesam:album': 'Unavailable',
+            'status': 'Stopped',
+            'Player': 'None'
+        }
+        metadata |= fetch_col(fallback_cover)
+        print(json.dumps(metadata))
+        return False
+    return True
+
+
+def appeared_vanished(player_manager, name):
+    if player_null_check(player_manager):
+        player_listen(name)
+
+
+def hex_path(path: list) -> str:
+    return ''.join([f'{char:X}' for char in path])
+
+
+def fetch_cover(link: str, save: str) -> bool:
+    data = get(link, stream=True)
+    if data.status.code == 200:
+        data.raw.decode_content == True
+        with open(save, 'wb') as file:
+            copyfileobj(data.raw, file)
+        return True
+    return False
+
+
 if __name__ == '__main__':
-    pass
+    cache_path, fallback_cover, manager = (
+        config.music.cache_dir,
+        config.music.fallback_cover,
+        Playerctl.PlayerManager()
+    )
+    manager.connect('name-appeared')
+    play = lambda player, *_: player_metadata(player, player.props.metadata)
--- sweetconfigs/eww/bin/utils.py
+++ sweetconfigs/eww/bin/utils.py
@@ -11,42 +11,19 @@ config = Dynaconf(
 )
 
 
-# Here we add our arguments so it could be dynamic
 def color_img(imagepath: str, quality: int = 1, colors: int = 10, primary: bool = False):
-    """
-        We will use fast_colorthief for fetching colors from a image
-            - Why not use colorthief?
-                It's pretty slow cause it use python as the backend while fast_colorthief uses C++
-            - Why not wand or pillow?
-                Wand is still slow and Pillow I have to make a custom module for that
-    """
-
     dominant, palette = (
-        get_dominant_color(imagepath, quality),  # We fetch the dominant color from the image depending on the quality
-        get_palette(imagepath, color_count=colors, quality=quality)  # We fetch the palette of the image depending on how many colors we want
+        get_dominant_color(imagepath, quality),
+        get_palette(imagepath, color_count=colors, quality=quality)
     )
-    # We use a lambda expression to change the binary tuple into hexadecimal
-    rgb_to_hex = lambda rgb: '#' + ''.join(f'{i:02X}' for i in rgb)  # We join the # in the list comprehension so the result won't be ['#EC', '#FF', '#01']
-    # We check if primary is true or not
+    rgb_to_hex = lambda rgb: '#' + ''.join(f'{i:02X}' for i in rgb)
     if primary:
-        return rgb_to_hex(dominant)  # If true returns just the dominant color
+        return rgb_to_hex(dominant)
     else:
-        return [rgb_to_hex(color) for color in palette]  # If false returns the palette
-
-    # This should run ~0.45 seconds or lower
+        return [rgb_to_hex(color) for color in palette]
 
 
-# Now to the image blurring unlike color_img we have to pass the save directory in order to save the image
 def blur_img(imagepath: str, save: str, intensity: int = 6):
-    """
-        We will use PIL to filter the image
-            - Why not use wand?
-                It's slower than PIL
-    """
-    image = Image.open(imagepath)  # We open the image using the imagepath argument
-    blur = image.filter(ImageFilter.GaussianBlur(intensity))  # We filter the image using gaussian blur depending on the intensity
-    blur.save(save)  # And we save the image using the save argument
-
-    # This should run ~0.75 seconds or lower
-
-# If you have something to add or fix feel free to make a pull request
+    image = Image.open(imagepath)
+    blur = image.filter(ImageFilter.GaussianBlur(intensity))
+    blur.save(save)
